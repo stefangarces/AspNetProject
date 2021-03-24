501e762533aeafd1b369496c4b884ac4aca3eb50
Stefan Garces <stefangarces@gmail.com>
2021-03-24 14:07:43 +0100

    Add: A class with the SeedData.cs, instead to have it in the program.cs.

--- AspNetProject/Program.cs
+++ AspNetProject/Program.cs
@@ -1,14 +1,9 @@
-using AspNetProject.Data;
+﻿using AspNetProject.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspNetProject.Models;
 
 namespace AspNetProject
 {
@@ -23,17 +18,14 @@ namespace AspNetProject
 
         private static void CreateDbifNotExist(IHost host)
         {
-            using(var scope = host.Services.CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
 
                 try
                 {
                     var context = services.GetRequiredService<AspNetProjectContext>();
-                    context.Attendee.RemoveRange(context.Attendee);
-                    context.Attendee.Add(new Attendee { UserName = "Bjorn", Email = "bjornstromberg@codic.se", PhoneNumber = "0700-000000" });
-                    context.SaveChanges();
-
+                    SeedData.StartSeed(context);
                 }
                 catch (Exception sten)
                 {
---
+++ AspNetProject/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using AspNetProject.Data;
+using AspNetProject.Models;
+using System;
+
+namespace AspNetProject
+{
+    public class SeedData
+    {
+        public static void StartSeed(AspNetProjectContext context)
+        {
+            context.Attendee.RemoveRange(context.Attendee);
+            context.Event.RemoveRange(context.Event);
+
+            context.Attendee.Add(new Attendee
+            {
+                UserName = "Bjorn",
+                Email = "bjornstromberg@codic.se",
+                PhoneNumber = "0700-001001"
+            });
+
+            context.Event.Add(new Event
+            {
+                Title = "Carola",
+                Description = "Fantastisk show!",
+                Place = "Annexet",
+                Adress = "Stockholmsgatan 14",
+                Date = DateTime.Parse("2022-05-22"),
+                TicketsAvailable = true
+            });
+            context.Event.Add(new Event
+            {
+                Title = "Evert Taube",
+                Description = "Otrolig show!",
+                Place = "Flen Arena",
+                Adress = "Flengatan 122",
+                Date = DateTime.Parse("2022-05-22"),
+                TicketsAvailable = true
+            });
+            context.Event.Add(new Event
+            {
+                Title = "Vikingarna",
+                Description = "Kom och njut av odödliga klassiker!",
+                Place = "Nya Ullevi",
+                Adress = "Göteborgsgatan 14",
+                Date = DateTime.Parse("2022-05-22"),
+                TicketsAvailable = true
+            });
+            context.Event.Add(new Event
+            {
+                Title = "BTS",
+                Description = "Pojkbandet från Korea har landet i Sverige!",
+                Place = "Ericsson Globe",
+                Adress = "Stockholmsgatan 22",
+                Date = DateTime.Parse("2024-02-27"),
+                TicketsAvailable = false
+            });
+            context.SaveChanges();
+        }
+    }
+}
